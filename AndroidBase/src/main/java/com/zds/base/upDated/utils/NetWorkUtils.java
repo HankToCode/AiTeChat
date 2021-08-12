23bfdebf3c73af4ae0d9beda85ff502757d95618
张荣东 <1063321593@qq.com>
2021-08-12 20:32:39 +0800

    feat:增加base基库 调整项目结构

---
+++ AndroidBase/src/main/java/com/zds/base/upDated/utils/NetWorkUtils.java
@@ -0,0 +1,89 @@
+package com.zds.base.upDated.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.LinkedList;
+
+import static android.Manifest.permission.INTERNET;
+
+import androidx.annotation.RequiresPermission;
+
+/**
+ * Created by cretin on 2017/4/21.
+ */
+
+public class NetWorkUtils {
+    public static String getCurrentNetType(Context context) {
+        String type = "";
+        ConnectivityManager cm = ( ConnectivityManager ) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if ( info == null ) {
+            type = "null";
+        } else if ( info.getType() == ConnectivityManager.TYPE_WIFI ) {
+            type = "wifi";
+        } else if ( info.getType() == ConnectivityManager.TYPE_MOBILE ) {
+            int subType = info.getSubtype();
+            if ( subType == TelephonyManager.NETWORK_TYPE_CDMA || subType == TelephonyManager.NETWORK_TYPE_GPRS
+                    || subType == TelephonyManager.NETWORK_TYPE_EDGE ) {
+                type = "2g";
+            } else if ( subType == TelephonyManager.NETWORK_TYPE_UMTS || subType == TelephonyManager.NETWORK_TYPE_HSDPA
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_A || subType == TelephonyManager.NETWORK_TYPE_EVDO_0
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_B ) {
+                type = "3g";
+            } else if ( subType == TelephonyManager.NETWORK_TYPE_LTE ) {// LTE是3g到4g的过渡，是3.9G的全球标准
+                type = "4g";
+            }
+        }
+        return type;
+    }
+
+    /**
+     * Return the ip address.
+     * <p>Must hold {@code <uses-permission android:name="android.permission.INTERNET" />}</p>
+     *
+     * @param useIPv4 True to use ipv4, false otherwise.
+     * @return the ip address
+     */
+    @RequiresPermission(INTERNET)
+    public static String getIPAddress(final boolean useIPv4) {
+        try {
+            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+            LinkedList<InetAddress> adds = new LinkedList<>();
+            while (nis.hasMoreElements()) {
+                NetworkInterface ni = nis.nextElement();
+                // To prevent phone of xiaomi return "10.0.2.15"
+                if (!ni.isUp() || ni.isLoopback()) continue;
+                Enumeration<InetAddress> addresses = ni.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    adds.addFirst(addresses.nextElement());
+                }
+            }
+            for (InetAddress add : adds) {
+                if (!add.isLoopbackAddress()) {
+                    String hostAddress = add.getHostAddress();
+                    boolean isIPv4 = hostAddress.indexOf(':') < 0;
+                    if (useIPv4) {
+                        if (isIPv4) return hostAddress;
+                    } else {
+                        if (!isIPv4) {
+                            int index = hostAddress.indexOf('%');
+                            return index < 0
+                                    ? hostAddress.toUpperCase()
+                                    : hostAddress.substring(0, index).toUpperCase();
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
