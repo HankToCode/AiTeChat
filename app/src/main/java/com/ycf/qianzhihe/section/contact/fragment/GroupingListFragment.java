44ad04413a1a51cb9b671826dcfaef583a2bf412
mars_ <mars_chu@163.com>
2021-08-29 18:00:52 +0800

    修复点击分组发起聊天闪退

--- app/src/main/java/com/ycf/qianzhihe/section/contact/fragment/GroupingListFragment.java
+++ app/src/main/java/com/ycf/qianzhihe/section/contact/fragment/GroupingListFragment.java
@@ -1,40 +1,28 @@
 package com.ycf.qianzhihe.section.contact.fragment;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import com.blankj.utilcode.util.LogUtils;
 import com.hyphenate.easeui.constants.EaseConstant;
 import com.ycf.qianzhihe.R;
-import com.ycf.qianzhihe.app.adapter.FriendGroupingAdapter;
 import com.ycf.qianzhihe.app.api.Constant;
 import com.ycf.qianzhihe.app.api.global.EventUtil;
 import com.ycf.qianzhihe.app.api.new_data.FriendGroupingBean;
 import com.ycf.qianzhihe.app.api.old_data.ContactListInfo;
 import com.ycf.qianzhihe.app.api.old_data.EventCenter;
-import com.ycf.qianzhihe.app.api.old_data.GroupInfo;
-import com.ycf.qianzhihe.app.api.old_data.MyGroupInfoList;
 import com.ycf.qianzhihe.app.api.old_http.ApiClient;
 import com.ycf.qianzhihe.app.api.old_http.AppConfig;
 import com.ycf.qianzhihe.app.api.old_http.ResultListener;
 import com.ycf.qianzhihe.app.base.BaseInitFragment;
-import com.ycf.qianzhihe.app.domain.EaseUser;
-import com.ycf.qianzhihe.app.help.RclViewHelp;
-import com.ycf.qianzhihe.app.operate.UserOperateManager;
-import com.ycf.qianzhihe.common.utils.ToastUtils;
 import com.ycf.qianzhihe.section.chat.activity.ChatActivity;
 import com.ycf.qianzhihe.section.contact.adapter.GroupingAdapter;
-import com.ycf.qianzhihe.section.contact.adapter.MyGroupAdapter;
 import com.zds.base.Toast.ToastUtil;
 import com.zds.base.json.FastJsonUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,7 +36,7 @@ public class GroupingListFragment extends BaseInitFragment {
     private boolean isFirstStart = true;
     //    private List<FriendGroupingBean> mGroupInfoList;
     private ExpandableListView elv_expand;
-    private ArrayList<String> mGroupingList;//分组数据
+    private ArrayList<String> mGroupingNameList;//分组数据
     //item成员数据
     private ArrayList<ArrayList<ContactListInfo.DataBean>> mItemSet;
     private GroupingAdapter mGroupingAdapter;
@@ -88,9 +76,9 @@ public class GroupingListFragment extends BaseInitFragment {
     protected void initData() {
         super.initData();
 //        mGroupInfoList = new ArrayList<>();
-        mGroupingList = new ArrayList<>();
+        mGroupingNameList = new ArrayList<>();
         mItemSet = new ArrayList<>();
-        mGroupingAdapter = new GroupingAdapter(mContext, mGroupingList, mItemSet);
+        mGroupingAdapter = new GroupingAdapter(mContext, mGroupingNameList, mItemSet);
         elv_expand.setAdapter(mGroupingAdapter);
         elv_expand.setGroupIndicator(null);//不设置大组指示器图标，因为我们自定义设置了
         elv_expand.setDivider(null);//设置图片可拉伸的
@@ -114,13 +102,14 @@ public class GroupingListFragment extends BaseInitFragment {
     }
 
 
-    private List<FriendGroupingBean> groupingDatas = new ArrayList<>();
+    private List<FriendGroupingBean> categoryDatas = new ArrayList<>();
     private void groupingList() {
         Map<String, Object> map = new HashMap<>(2);
         ApiClient.requestNetHandle(requireContext(), AppConfig.selectFriendCategory, "", map, new ResultListener() {
             @Override
             public void onSuccess(String json, String msg) {
-                groupingDatas = FastJsonUtil.getList(json, FriendGroupingBean.class);
+                categoryDatas.clear();
+                categoryDatas = FastJsonUtil.getList(json, FriendGroupingBean.class);
                 /*if (page == 0 && mGroupInfoList.size() > 0) {
                     mGroupInfoList.clear();
                 }
@@ -140,7 +129,6 @@ public class GroupingListFragment extends BaseInitFragment {
         Map<String, Object> map = new HashMap<>(2);
         map.put("pageNum", 1);
         map.put("pageSize", 10000);
-
         //未同步通讯录到本地
         ApiClient.requestNetHandle(getActivity(), AppConfig.USER_FRIEND_LIST,
                 "", map, new ResultListener() {
@@ -150,25 +138,20 @@ public class GroupingListFragment extends BaseInitFragment {
                                 ContactListInfo.class);
                         List<ContactListInfo.DataBean> mContactList = new ArrayList<>();
                         mContactList.addAll(info.getData());
-
-                        mGroupingList.clear();//分组名
+                        mGroupingNameList.clear();//分组名
                         mItemSet.clear();//分组成员
                         //读取本地缓存好友数据
 //                        List<ContactListInfo.DataBean> localFriendList = UserOperateManager.getInstance().getContactList();
-
-                        if (groupingDatas.size() > 0) {
-                            for (int i = 0; i < groupingDatas.size(); i++) {
-                                mGroupingList.add(groupingDatas.get(i).getName());
-                                ArrayList<ContactListInfo.DataBean> itemList1 = new ArrayList<>();
-
-                                for (int j = 0; j < mContactList.size(); j++) {
-                                    if (groupingDatas.get(i).getCategoryId().equals(mContactList.get(j).getCategoryId())) {
-                                        itemList1.add(mContactList.get(i));//成员
-                                        mItemSet.add(itemList1);
+                        if (categoryDatas.size() > 0) {
+                            for (int i = 0; i < categoryDatas.size(); i++) {
+                                mGroupingNameList.add(categoryDatas.get(i).getName());
+                                ArrayList<ContactListInfo.DataBean> item = new ArrayList<>();
+                                for (int k = 0; k < mContactList.size(); k++) {
+                                    if (categoryDatas.get(i).getCategoryId().equals(mContactList.get(k).getCategoryId())) {
+                                        item.add(mContactList.get(k));//成员
                                     }
                                 }
-//                        itemList1.add(mGroupInfoList.get(i).getUserId());//成员
-//                        mItemSet.add(itemList1);
+                                mItemSet.add(item);
                             }
                         }
                         mGroupingAdapter.notifyDataSetChanged();
