710ffeebc2ae80938bd7c8584f920d4657cfc2ac
mars_ <mars_chu@163.com>
2021-09-17 19:56:29 +0800

    更换接口域名 修改充值最大金额

--- app/src/main/java/com/ycf/qianzhihe/section/common/RechargeActivity.java
+++ app/src/main/java/com/ycf/qianzhihe/section/common/RechargeActivity.java
@@ -119,7 +119,7 @@ public class RechargeActivity extends BaseInitActivity {
         });
 
         //初始化金额选择
-        cml_money.setMoneyData(new int[]{30, 50, 100, 200, 500, 3000});
+        cml_money.setMoneyData(new int[]{30, 50, 100, 200, 500, 2000});
         cml_money.setDefaultPositon(-1);
         cml_money.setOnChoseMoneyListener(new ChooseMoneyLayout.onChoseMoneyListener() {
             @Override
