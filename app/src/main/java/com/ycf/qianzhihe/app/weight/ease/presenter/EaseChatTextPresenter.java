01baa0b379f64c94bca92f5bcd50bcd175a8c726
张荣东 <1063321593@qq.com>
2021-11-30 18:24:26 +0800

    fix:修复删除会话问题

--- app/src/main/java/com/ycf/qianzhihe/app/weight/ease/presenter/EaseChatTextPresenter.java
+++ app/src/main/java/com/ycf/qianzhihe/app/weight/ease/presenter/EaseChatTextPresenter.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.widget.BaseAdapter;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMMessage;
+import com.ycf.qianzhihe.app.api.global.EventUtil;
+import com.ycf.qianzhihe.app.api.old_data.EventCenter;
 import com.ycf.qianzhihe.app.weight.ease.chatrow.EaseChatRow;
 import com.ycf.qianzhihe.app.weight.ease.chatrow.EaseChatRowText;
 import com.hyphenate.exceptions.HyphenateException;
+import com.zds.base.json.FastJsonUtil;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * Created by zhangsong on 17-10-12.
@@ -28,6 +34,17 @@ public class EaseChatTextPresenter extends EaseChatRowPresenter {
             } catch (HyphenateException e) {
                 e.printStackTrace();
             }
+        }
+
+        try {
+            EMConversation conversation = EMClient.getInstance().chatManager().getConversation(message.conversationId());
+            String json = FastJsonUtil.toJSONString(conversation.getLastMessage().ext());
+            if (json.contains("msgType") && "deleteuser".equals(FastJsonUtil.getString(json, "msgType"))) {
+//                EMClient.getInstance().chatManager().deleteConversation(message.conversationId(), true);
+                EventBus.getDefault().post(new EventCenter<>(EventUtil.DELETE_CONTACT));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
