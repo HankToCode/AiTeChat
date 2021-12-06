67b86a3f360315d4a1312ae5cc6d768cfe48e14d
张荣东 <1063321593@qq.com>
2021-12-06 21:40:24 +0800

    发现

--- app/src/main/java/com/ycf/qianzhihe/section/discover/DiscoverFragment.java
+++ app/src/main/java/com/ycf/qianzhihe/section/discover/DiscoverFragment.java
@@ -1,55 +1,46 @@
 package com.ycf.qianzhihe.section.discover;
 
-import android.content.Context;
+import static com.darsh.multipleimageselect.helpers.Constants.REQUEST_CODE;
+
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.baidu.mapapi.common.SysOSUtil;
-import com.bumptech.glide.Glide;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.ycf.qianzhihe.MainActivity;
 import com.ycf.qianzhihe.R;
-import com.ycf.qianzhihe.app.api.new_data.ImageListBean;
+import com.ycf.qianzhihe.app.api.Constant;
+import com.ycf.qianzhihe.app.api.Global;
 import com.ycf.qianzhihe.app.base.BaseInitFragment;
 import com.ycf.qianzhihe.app.base.WebViewActivity;
+import com.ycf.qianzhihe.app.operate.UserOperateManager;
 import com.ycf.qianzhihe.common.widget.BannerImageLoader;
-import com.youth.banner.Banner;
+import com.ycf.qianzhihe.section.account.activity.UserInfoDetailActivity;
+import com.ycf.qianzhihe.section.contact.activity.AddUserActivity;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.Transformer;
 import com.youth.banner.listener.OnBannerListener;
-import com.youth.banner.loader.ImageLoader;
-import com.youth.banner.loader.ImageLoaderInterface;
+import com.zds.base.code.activity.CaptureActivity;
 
 import java.util.ArrayList;
 
-import butterknife.BindView;
 import butterknife.OnClick;
 
-public class DiscoverFragment extends BaseInitFragment implements OnBannerListener {
+public class DiscoverFragment extends BaseInitFragment implements View.OnClickListener {
 
-    @BindView(R.id.ll_dc1)
-    LinearLayout iv_dc1;
-    @BindView(R.id.ll_dc2)
-    LinearLayout iv_dc2;
-    @BindView(R.id.ll_dc3)
-    LinearLayout iv_dc3;
-    @BindView(R.id.ll_dc4)
-    LinearLayout iv_dc4;
-    @BindView(R.id.ll_dc5)
-    LinearLayout iv_dc5;
-    @BindView(R.id.ll_dc6)
-    LinearLayout iv_dc6;
-    @BindView(R.id.banner)
-    Banner banner;
-    private ArrayList<Integer> list_path;
-    private ArrayList<String> list_title;
+
+    private ConstraintLayout mClSYS;
+    private ImageView mIvSYS;
+    private ConstraintLayout mClKYK;
+    private ImageView mIvKYK;
+    private ConstraintLayout mClYQ;
+    private ImageView mIvYQ;
+    private ConstraintLayout mClQRC;
+    private ImageView mIvQRC;
+    private ConstraintLayout mClWYW;
+    private ImageView mIvWYW;
 
     @Override
     protected int getLayoutId() {
@@ -59,55 +50,74 @@ public class DiscoverFragment extends BaseInitFragment implements OnBannerListen
     @Override
     protected void initView(Bundle savedInstanceState) {
         super.initView(savedInstanceState);
-        //放图片地址的集合
-        list_path = new ArrayList<>();
-        list_title = new ArrayList<>();
-        list_path.add(R.mipmap.qznn);
-        list_path.add(R.drawable.img_b1);
-        list_path.add(R.drawable.img_b2);
-        list_title.add("不搞对象");
-        list_title.add("热爱劳动");
-        list_title.add("好好学习");
-        banner.setImages(list_path);
-        banner.setBannerTitles(list_title);
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
-        banner.setImageLoader(new BannerImageLoader());
-        banner.setBannerAnimation(Transformer.DepthPage);
-        banner.setDelayTime(3000);
-        banner.isAutoPlay(true);
-        banner.setIndicatorGravity(BannerConfig.CENTER).setOnBannerListener(this).start();
+        mClSYS = (ConstraintLayout) findViewById(R.id.clSYS);
+        mIvSYS = (ImageView) findViewById(R.id.ivSYS);
+        mClKYK = (ConstraintLayout) findViewById(R.id.clKYK);
+        mIvKYK = (ImageView) findViewById(R.id.ivKYK);
+        mClYQ = (ConstraintLayout) findViewById(R.id.clYQ);
+        mIvYQ = (ImageView) findViewById(R.id.ivYQ);
+        mClQRC = (ConstraintLayout) findViewById(R.id.clQRC);
+        mIvQRC = (ImageView) findViewById(R.id.ivQRC);
+        mClWYW = (ConstraintLayout) findViewById(R.id.clWYW);
+        mIvWYW = (ImageView) findViewById(R.id.ivWYW);
     }
 
     @Override
-    public void OnBannerClick(int position) {
+    protected void initListener() {
+        super.initListener();
+        mClSYS.setOnClickListener(this);
+        mClKYK.setOnClickListener(this);
+        mClYQ.setOnClickListener(this);
+        mClQRC.setOnClickListener(this);
+        mClWYW.setOnClickListener(this);
 
     }
 
-    @OnClick({R.id.ll_dc1, R.id.ll_dc2, R.id.ll_dc3, R.id.ll_dc4, R.id.ll_dc5, R.id.ll_dc6})
-    public void click(View v) {
-        String url = "";
+    @Override
+    public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.ll_dc1:
-//                url = "http://meishi.meituan.com/i/?ci=268&stid_b=1&cevent=imt%2Fhomepage%2Fcategory1%2F1";
-                url = "https://www.meituan.com/";
+            case R.id.clSYS:
+                AddUserActivity.actionStart(requireContext());
                 break;
-            case R.id.ll_dc2:
-                url = "https://h5.ele.me/";
+            case R.id.clKYK:
                 break;
-            case R.id.ll_dc3:
-                url = "https://www.jd.com/brand/13196fb3651ed8a45efe.html";
+            case R.id.clYQ:
+
                 break;
-            case R.id.ll_dc4:
-                url = "https://s.taobao.com/";
+            case R.id.clQRC:
+                Global.addUserOriginType = Constant.ADD_USER_ORIGIN_TYPE_QRCODE;
+                Intent intent = new Intent(requireContext(),
+                        CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
                 break;
-            case R.id.ll_dc5:
-                url = "https://news.qq.com/";
-                break;
-            case R.id.ll_dc6:
-                url = "https://new.qq.com/ch/cul/";
+            case R.id.clWYW:
+
                 break;
         }
-        startActivity(new Intent(mContext, WebViewActivity.class).putExtra("title", "lan").putExtra("url", url));
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            //处理扫描结果（在界面上显示）
+            if (null != data) {
+                Bundle bundle = data.getExtras();
+                if (bundle == null) {
+                    return;
+                }
+                String result = bundle.getString(CaptureActivity.INTENT_EXTRA_KEY_QR_SCAN);
+
+                if (result.contains("person")) {
+                    startActivity(new Intent(mContext,
+                            UserInfoDetailActivity.class).putExtra(
+                            "friendUserId", result.split("_")[0]));
+                } else if (result.contains("group")) {
+                    UserOperateManager.getInstance().scanInviteContact(mContext, result);
+                }
+            }
+        }
     }
 
 
