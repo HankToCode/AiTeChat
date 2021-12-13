113e9ba9eb63e15568a998c4e3dbb79616d3f264
张荣东 <1063321593@qq.com>
2021-12-14 02:29:16 +0800

    好友详情完成

--- app/src/main/java/com/ycf/qianzhihe/section/account/activity/UserInfoDetailActivity.java
+++ app/src/main/java/com/ycf/qianzhihe/section/account/activity/UserInfoDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -41,6 +42,7 @@ import com.ycf.qianzhihe.app.utils.ProjectUtil;
 import com.ycf.qianzhihe.app.utils.hxSetMessageFree.EaseSharedUtils;
 import com.ycf.qianzhihe.app.utils.my.MyHelper;
 import com.ycf.qianzhihe.app.weight.MyDialog;
+import com.ycf.qianzhihe.common.widget.UserInfoMoreWindow;
 import com.ycf.qianzhihe.section.chat.activity.ChatActivity;
 import com.ycf.qianzhihe.section.common.MyQrActivity;
 import com.ycf.qianzhihe.section.conversation.ChatBgActivity;
@@ -59,6 +61,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.OnClick;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 /**
  * @author lhb
@@ -83,12 +87,10 @@ public class UserInfoDetailActivity extends BaseInitActivity {
     private TextView mTvAccount;
     private TextView mTvDescription;
     private SuperTextView mTvToTop;
-    private SuperTextView mTvMute;
     private LinearLayout mLlBottomFriend;
     private SuperTextView mTvDelFriend;
     private SuperTextView mTvSendMsg;
     private SuperTextView mTvAddFriend;
-    private SuperTextView mTvKickOut;
     private SuperTextView mTvFree;
 
 
@@ -108,24 +110,108 @@ public class UserInfoDetailActivity extends BaseInitActivity {
         mTvAccount = (TextView) findViewById(R.id.tv_account);
         mTvDescription = (TextView) findViewById(R.id.tv_description);
         mTvToTop = (SuperTextView) findViewById(R.id.tv_to_top);
-        mTvMute = (SuperTextView) findViewById(R.id.tv_mute);
         mLlBottomFriend = (LinearLayout) findViewById(R.id.ll_bottom_friend);
         mTvDelFriend = (SuperTextView) findViewById(R.id.tv_del_friend);
         mTvSendMsg = (SuperTextView) findViewById(R.id.tv_send_msg);
         mTvAddFriend = (SuperTextView) findViewById(R.id.tv_add_friend);
-        mTvKickOut = (SuperTextView) findViewById(R.id.tv_kick_out);
         mTvFree = (SuperTextView) findViewById(R.id.tv_free);
 
         initLogic();
 
     }
 
+    //是否禁言
+    private boolean isMute = false;
+    //是否拉黑
+    private boolean isBlack = false;
+    //是否朋友
+    private boolean isFriend = false;
+    //是否管理员
+    private boolean isUserRank = false;
+
+    public interface UserInfoDetailInfoInterface {
+        boolean isMute();
+
+        boolean isBlack();
+
+        boolean isFriend();
+
+        boolean isUserRank();
+    }
+
     protected void initLogic() {
 
         initImmersionBar(false);
         mTitleBar.setOnBackPressListener(view -> finish());
-        mTitleBar.setOnRightClickListener(view -> {
+        mTitleBar.setRightLayoutClickListener(view -> {
             //弹出
+            UserInfoMoreWindow userInfoMoreWindow = new UserInfoMoreWindow(this, integer -> {
+                switch (integer) {
+                    case 1:
+                    case 7:
+                        //禁言
+                        modifyGroupUserSayStatus(!isMute);
+                        break;
+                    case 2:
+                    case 8:
+                        //移出群组
+                        delGroupUser();
+                        break;
+                    case 3:
+                        //查找聊天记录
+                        startActivity(new Intent(UserInfoDetailActivity.this, ChatRecordActivity.class).putExtra("chatId", userId + Constant.ID_REDPROJECT));
+                        break;
+                    case 4:
+                        //分组
+                        FriendGroupingActvity.actionStart(mContext, userId, info.getCategoryId(), info.getCategoryName());
+                        break;
+                    case 5:
+                        if (info.getFriendFlag().equals("1")) {
+                            if (info.getBlackStatus() != null && info.getBlackStatus().equals("1")) {
+                                isBlack = false;
+                                blackContact("0");
+                            } else {
+                                isBlack = true;
+                                blackContact("1");
+                            }
+                        } else {
+                            isFriend = false;
+                        }
+                        break;
+                    case 6:
+                    case 9:
+                        if (from.equals("1")) {
+                            startActivity(new Intent(this, ReportActivity.class).putExtra("from", from).putExtra("userGroupId", userId));
+                        } else if (from.equals("2")) {
+                            startActivity(new Intent(this, ReportActivity.class).putExtra("from", from).putExtra("userGroupId", groupId));
+                        }
+                        break;
+                }
+
+                return null;
+            }, new UserInfoDetailInfoInterface() {
+
+                @Override
+                public boolean isMute() {
+                    return isMute;
+                }
+
+                @Override
+                public boolean isBlack() {
+                    return isBlack;
+                }
+
+                @Override
+                public boolean isFriend() {
+                    return isFriend;
+                }
+
+                @Override
+                public boolean isUserRank() {
+                    return isUserRank;
+                }
+            });
+            userInfoMoreWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
         });
         /*mImgRight.setOnClickListener(v ->
                 startActivity(new Intent(UserInfoDetailActivity.this, ChatMoreSetlActivity.class)
@@ -137,10 +223,10 @@ public class UserInfoDetailActivity extends BaseInitActivity {
 
         if (chatType == Constant.CHATTYPE_GROUP) {
             if (currentUserRank == 2 || currentUserRank == 1) {//2群主  1管理员
-                mTvDelFriend.setText("踢出群组");
+                isUserRank = true;
             }
         } else {
-            mTvDelFriend.setText("删除好友");
+            isUserRank = false;
         }
 
         if (userId.contains(UserComm.getUserId())) {
@@ -153,15 +239,14 @@ public class UserInfoDetailActivity extends BaseInitActivity {
             mTvAccount.setText(loginInfo.getUserCode());
             mLlBottomFriend.setVisibility(View.GONE);
             mTvRemark.setVisibility(View.GONE);
-            mTvMute.setVisibility(View.GONE);
             mTvFree.setVisibility(View.GONE);
-            mTvKickOut.setVisibility(View.GONE);
             mTvAddFriend.setVisibility(View.GONE);
         } else {
             if (!TextUtils.isEmpty(emGroupId)) {
                 getGroupMuteList();
-                mTvMute.setVisibility(View.VISIBLE);
             }
+
+            mTvRemark.setVisibility(View.VISIBLE);
         }
         queryFriendInfo();
 
@@ -197,15 +282,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                         null, "true", null);
             }
         });
-
-        //加入黑名单
-       /* mSwitchBlack.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                blackContact("1");
-            } else {
-                blackContact("0");
-            }
-        });*/
 
         EMConversation emConversation = EMClient.getInstance().chatManager().getConversation(userId + Constant.ID_REDPROJECT);
         if (emConversation != null) {
@@ -244,14 +320,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                 switchStart("0");
             }
         });*/
-    }
-
-    private void setMuteListener() {
-        mTvMute.setOnClickListener(view -> {
-            view.setSelected(!view.isSelected());
-            modifyGroupUserSayStatus(view.isSelected());
-
-        });
     }
 
     /*private void addRemark() {
@@ -309,24 +377,20 @@ public class UserInfoDetailActivity extends BaseInitActivity {
             public void onSuccess(Map<String, Long> stringLongMap) {
                 for (String key : stringLongMap.keySet()) {
                     if (key.contains(userId))
-                        runOnUiThread(() -> mTvMute.setSelected(true));
+                        runOnUiThread(() -> isMute = true);
                 }
-
-                runOnUiThread(() -> setMuteListener());
-
 
             }
 
             @Override
             public void onError(int i, String s) {
-                runOnUiThread(() -> setMuteListener());
             }
         });
 
     }
 
     private void modifyGroupUserSayStatus(boolean isMute) {
-
+        this.isMute = isMute;
         Map<String, Object> map = new HashMap<>(1);
         if (userId.contains(Constant.ID_REDPROJECT)) {
             userId = userId.split("-")[0];
@@ -349,28 +413,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                     public void onFailure(String msg) {
                     }
                 });
-
-//        List<String> muteMembers = new ArrayList<>();
-//        muteMembers.add(friendUserId+Constant.ID_REDPROJECT);
-//        if (isMute) {
-//
-//            EMClient.getInstance().groupManager().aysncMuteGroupMembers(emGroupId, muteMembers, 12*30*24*60*60*1000, new EMValueCallBack<EMGroup>() {
-//                @Override
-//                public void onSuccess(EMGroup emGroup) {}
-//
-//                @Override
-//                public void onError(int i, String s) {}
-//            });
-//        }else {
-//            EMClient.getInstance().groupManager().asyncUnMuteGroupMembers(emGroupId, muteMembers, new EMValueCallBack<EMGroup>() {
-//                @Override
-//                public void onSuccess(EMGroup emGroup) {}
-//
-//                @Override
-//                public void onError(int i, String s) {}
-//            });
-//        }
-
 
     }
 
@@ -427,13 +469,14 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                                 } else {
                                     iv_start.setVisibility(View.GONE);
                                     switch_start.setChecked(false);
-                                }
-                                ll_grouping.setVisibility(View.VISIBLE);
-                                tv_grouping.setText(info.getCategoryName());*/
+                                }*/
+                                isFriend = true;
                                 if (info.getBlackStatus().equals("1")) {
+                                    isBlack = true;
                                     //黑名单中
                                     mLlBottomFriend.setVisibility(View.GONE);
                                 } else {
+                                    isBlack = false;
                                     mLlBottomFriend.setVisibility(View.VISIBLE);
                                 }
                                 try {
@@ -442,10 +485,13 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                                     e.printStackTrace();
                                 }
                             } else {
+                                isFriend = false;
                                 if (userId.contains(UserComm.getUserId())) {//自己
                                     mLlBottomFriend.setVisibility(View.GONE);
+                                    mTvAddFriend.setVisibility(View.GONE);
                                 } else {
-                                    mLlBottomFriend.setVisibility(View.VISIBLE);
+                                    mLlBottomFriend.setVisibility(View.GONE);
+                                    mTvAddFriend.setVisibility(View.VISIBLE);
                                 }
                             }
                             /*if (!TextUtils.isEmpty(inviterUserId)) {
@@ -512,20 +558,12 @@ public class UserInfoDetailActivity extends BaseInitActivity {
     }
 
 
-    @OnClick({R.id.tv_send_msg, R.id.tv_add_friend, R.id.tv_kick_out, R.id.tv_remark, R.id.tv_del_friend, R.id.iv_qr})
+    @OnClick({R.id.tv_send_msg, R.id.tv_add_friend, R.id.tv_remark, R.id.tv_del_friend, R.id.iv_qr})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_qr:
                 MyQrActivity.actionStart(mContext);
                 break;
-            /*case R.id.ll_grouping:
-                Intent intent1 = new Intent(mContext, FriendGroupingActvity.class);
-                intent1.putExtra("friendUserId", userId);
-                intent1.putExtra("categoryId", info.getCategoryId());
-                intent1.putExtra("categoryName", info.getCategoryName());
-                startActivityForResult(intent1, 301);
-//                FriendGroupingActvity.actionStart(mContext,userId,info.getCategoryId(),info.getCategoryName());
-                break;*/
             case R.id.tv_del_friend:
                 new MyDialog(this)
                         .setTitle("删除联系人")
@@ -540,10 +578,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                         .show();
 
                 break;
-            case R.id.tv_report:
-                //举报
-                startActivity(new Intent(this, ReportActivity.class).putExtra("from", "2").putExtra("userGroupId", groupId));
-                break;
             case R.id.tv_chat_bg:
                 //聊天背景
                 ChatBgActivity.actionStart(this, "1", info.getUserId() + Constant.ID_REDPROJECT);
@@ -556,15 +590,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                         clearSingleChatHistory();
                     }
                 }, true).show();
-                break;
-            case R.id.tv_kick_out:
-                delGroupUser();
-                break;
-
-            case R.id.tv_chat_record:
-                //查找聊天记录
-                startActivity(new Intent(this, ChatRecordActivity.class).putExtra("chatId", userId + Constant.ID_REDPROJECT));
-
                 break;
             case R.id.tv_send_msg:
                 //好友标识 0 不是好友 1 是好友
@@ -603,19 +628,6 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                 //加好友
                 addUser();
                 break;
-
-           /* //好友拉黑状态
-            if (info.getFriendFlag().equals("1")) {
-                if (info.getBlackStatus() != null && info.getBlackStatus().equals("1")) {
-                    blackContact("0");
-                }
-            } else {
-            }*/
-
-//            case R.id.tv_report:
-//                //举报
-//                startActivity(new Intent(this, ReportActivity.class).putExtra("from", "1").putExtra("userGroupId", userId));
-//                break;
             case R.id.tv_remark:
                 if (info == null) {
                     return;
@@ -694,36 +706,18 @@ public class UserInfoDetailActivity extends BaseInitActivity {
     /**
      * 解除拉黑好友
      */
-    /*public void blackContact(String blackStatus) {
-     *//* Map<String, Object> map = new HashMap<>(2);
-        map.put("friendUserId", userId);
-        //拉黑状态 0-未拉黑 1-已拉黑
-        map.put("blackStatus", "0");
-        ApiClient.requestNetHandle(this, AppConfig.BLACK_USER_FRIEND,
-                "正在移除黑名单...", map, new ResultListener() {
-                    @Override
-                    public void onSuccess(String json, String msg) {
-                        EventBus.getDefault().post(new EventCenter<>(EventUtil.REFRESH_BLACK));
-                        ToastUtil.toast(" 移除成功");
-                        finish();
-                    }
-
-                    @Override
-                    public void onFailure(String msg) {
-                        ToastUtil.toast(msg);
-                    }
-                });*//*
+    public void blackContact(String blackStatus) {
         Map<String, Object> map = new HashMap<>(2);
         map.put("friendUserId", userId.split("-")[0]);
         //拉黑状态 0-未拉黑 1-已拉黑
         map.put("blackStatus", blackStatus);
 
         ApiClient.requestNetHandle(this, AppConfig.BLACK_USER_FRIEND,
-                mSwitchBlack.isChecked() ? "正在拉黑..." : "正在取消拉黑...", map,
+                isBlack ? "正在拉黑..." : "正在取消拉黑...", map,
                 new ResultListener() {
                     @Override
                     public void onSuccess(String json, String msg) {
-                        if (mSwitchBlack.isChecked()) {
+                        if (isBlack) {
                             ToastUtil.toast("拉黑成功");
                             EMClient.getInstance().chatManager().deleteConversation(userId.contains(Constant.ID_REDPROJECT) ? userName : userId + Constant.ID_REDPROJECT, false);
                             EventBus.getDefault().post(new EventCenter<>(EventUtil.OPERATE_BLACK));
@@ -733,18 +727,18 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                             ToastUtil.toast("移除成功");
                             finish();
                         }
-                        *//*if (from.equals("3")) {
+                        if (from.equals("3")) {
                             EventBus.getDefault().post(new EventCenter<>(EventUtil.REFRESH_BLACK));
                             ToastUtil.toast(" 移除成功");
                             finish();
                         } else {
-                            if (mSwitchBlack.isChecked()) {
+                            if (isBlack) {
                                 ToastUtil.toast("拉黑成功");
                                 EMClient.getInstance().chatManager().deleteConversation(userId.contains(Constant.ID_REDPROJECT) ? userName : userId + Constant.ID_REDPROJECT, false);
                                 EventBus.getDefault().post(new EventCenter<>(EventUtil.OPERATE_BLACK));
                                 finish();
                             }
-                        }*//*
+                        }
                     }
 
                     @Override
@@ -752,7 +746,9 @@ public class UserInfoDetailActivity extends BaseInitActivity {
                         ToastUtil.toast(msg);
                     }
                 });
-    }*/
+    }
+
+
     public void switchStart(String status) {
         Map<String, Object> map = new HashMap<>(2);
         map.put("friendUserId", userId.split("-")[0]);
